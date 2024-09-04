f8bd862f5d7ac2f07522aed632e37d8a498f89e6
ChaosRifle <DC3120@gmail.com>
2024-09-04 14:36:29 -0600

    Update CONFIG_%MissionName%.lua

--- scripts/CONFIG_%MissionName%.lua
+++ scripts/CONFIG_%MissionName%.lua
@@ -1,9 +1,9 @@
 
 ---------------------------------------------------------------CarrierConfigurator---------------------------------------------------------------
 -----------------------------------------------------------------Required Config-----------------------------------------------------------------
-SuperCarrierID = 1128 --UnitID as found in mission editor
-LHATarawaID = 1070    --UnitID as found in mission editor
+SuperCarrierID = 1128                                                                     -- UnitID as found in mission editor
+LHATarawaID = 1070                                                                        -- UnitID as found in mission editor
 -----------------------------------------------------------------Optional Config-----------------------------------------------------------------
---CarrierConfiguratorConfig_initialSuperCarrierConfig = { 'Corral', 'EngineTest', 'Finger', 'FingerExtra', 'IslandCrew', 'IslandParkingAISafe', 'IslandParkingAIUnsafe', 'Junkyard', 'LSO', 'Patio', 'Point', 'Sixpack' }
---CarrierConfiguratorConfig_initialLHAConfig = { 'Bow-1980s', 'Junkyard-1980s', 'Stern-UnblockedHarrier-1980s' }
---CarrierConfiguratorConfig_templateSubDir = 'carrierconfiguration/'
+-- CarrierConfiguratorConfig_initialSuperCarrierConfig = { 'Corral', 'EngineTest', 'Finger', 'FingerExtra', 'IslandCrew', 'IslandParkingAISafe', 'IslandParkingAIUnsafe', 'Junkyard', 'LSO', 'Patio', 'Point', 'Sixpack' }
+-- CarrierConfiguratorConfig_initialLHAConfig = { 'Bow-1980s', 'Junkyard-1980s', 'Stern-UnblockedHarrier-1980s' }
+-- CarrierConfiguratorConfig_templateSubDir = 'carrierconfiguration/'
