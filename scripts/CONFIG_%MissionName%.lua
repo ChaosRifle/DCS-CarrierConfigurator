e67972535cefb9d75ed149d89de0a1a30eec794b
ChaosRifle <DC3120@gmail.com>
2024-09-11 00:37:20 -0600

    Update CONFIG_%MissionName%.lua

--- scripts/CONFIG_%MissionName%.lua
+++ scripts/CONFIG_%MissionName%.lua
@@ -1,9 +1,10 @@
-
----------------------------------------------------------------CarrierConfigurator---------------------------------------------------------------
------------------------------------------------------------------Required Config-----------------------------------------------------------------
+-------------------------------------------------------------- CarrierConfigurator --------------------------------------------------------------
+---------------------------------------------------------------- Required Config ----------------------------------------------------------------
 SuperCarrierID = 1128                                                                     -- UnitID as found in mission editor
 LHATarawaID = 1070                                                                        -- UnitID as found in mission editor
------------------------------------------------------------------Optional Config-----------------------------------------------------------------
+---------------------------------------------------------------- Optional Config ----------------------------------------------------------------
 -- CarrierConfiguratorConfig_initialSuperCarrierConfig = { 'Corral', 'EngineTest', 'Finger', 'FingerExtra', 'IslandCrew', 'IslandParkingAISafe', 'IslandParkingAIUnsafe', 'Junkyard', 'LSO', 'Patio', 'Point', 'Sixpack' }
 -- CarrierConfiguratorConfig_initialLHAConfig = { 'Bow-1980s', 'Junkyard-1980s', 'Stern-UnblockedHarrier-1980s' }
 -- CarrierConfiguratorConfig_templateSubDir = 'carrierconfiguration/'
+
+------------------------------------------------------------ End CarrierConfigurator ------------------------------------------------------------
