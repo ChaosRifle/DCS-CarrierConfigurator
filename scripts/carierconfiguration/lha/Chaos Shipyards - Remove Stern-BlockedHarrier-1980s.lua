2d89ce4c6dda9a110d94238c84c0e35bf00549c0
ChaosRifle <DC3120@gmail.com>
2024-10-03 06:05:33 -0600

    Delete scripts/carierconfiguration directory

--- scripts/carierconfiguration/lha/Chaos Shipyards - Remove Stern-BlockedHarrier-1980s.lua
+++
@@ -1,9 +0,0 @@
-do
-local statObj = coalition.getStaticObjects(2) 					--1 Red coalition 2 blue coalition
-	for i, static in pairs(statObj) do
-		local staticName = static:getName()
-			if string.match(staticName, "Chaos Shipyards LHA Stern-BlockedHarrier-1980s.*") then
-				static:destroy()				-- removes the object
-			end
-	end
-end
--- scripts/carierconfiguration/supercarrier/Chaos Shipyards - Remove ALL.lua
+++
@@ -1,9 +0,0 @@
-do
-local statObj = coalition.getStaticObjects(2) 					--1 Red coalition 2 blue coalition
-	for i, static in pairs(statObj) do
-		local staticName = static:getName()
-			if string.match(staticName, "Chaos Shipyards SC.*") then 		-- Searches for anything with a name starting with DeleteMe
-				static:destroy()				-- removes the object
-			end
-	end
-end
